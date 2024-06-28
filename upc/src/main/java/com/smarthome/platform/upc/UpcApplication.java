24077693b04386567d4535fddf4763da38e49525
Piero Delgado <112528610+PieroD04@users.noreply.github.com>
2024-06-28 10:58:24 -0500

    feat: base project

---
+++ upc/src/main/java/com/smarthome/platform/upc/UpcApplication.java
@@ -0,0 +1,13 @@
+package com.smarthome.platform.upc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpcApplication.class, args);
+    }
+
+}
---
+++ upc/src/test/java/com/smarthome/platform/upc/UpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthome.platform.upc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
